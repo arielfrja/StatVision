0fc13198cebaa1845a8400c8a56fe9759eaeb3d2
YOUR_GITHUB_USERNAME <arielfrja@gmail.com>
2026-07-07 14:20:27 +0300

    fix: add JSX type declarations for MWC custom elements to fix tsc --noEmit

---
+++ frontend/src/types/mwc.d.ts
@@ -0,0 +1,25 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'md-checkbox': any;
+    'md-circular-progress': any;
+    'md-dialog': any;
+    'md-divider': any;
+    'md-elevated-card': any;
+    'md-filled-button': any;
+    'md-filled-select': any;
+    'md-icon': any;
+    'md-icon-button': any;
+    'md-linear-progress': any;
+    'md-list': any;
+    'md-list-item': any;
+    'md-navigation-bar': any;
+    'md-navigation-tab': any;
+    'md-outlined-button': any;
+    'md-outlined-card': any;
+    'md-outlined-text-field': any;
+    'md-primary-tab': any;
+    'md-select-option': any;
+    'md-tabs': any;
+    'md-text-button': any;
+  }
+}
